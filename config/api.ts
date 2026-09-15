fe1cbaada457e79b802fac0b1176dcb48fd1cf6a
Liezel Chetty <liezel@designovation.ie>
2026-09-15 10:58:01 +0100

    Use stable Calvary podcast API URL

--- config/api.ts
+++ config/api.ts
@@ -1,7 +1,7 @@
 import { Platform } from 'react-native';
 
 // Replace this single value when the app moves to its permanent domain.
-export const publicAppBaseUrl = 'https://calvary-waterford-bc7hwtstj-liezels-projects-bab2d1e6.vercel.app';
+export const publicAppBaseUrl = 'https://calvary-waterford-app.vercel.app';
 export const podcastFeedUrl = Platform.select({
   web: '/api/podcast-feed',
   default: publicAppBaseUrl + '/api/podcast-feed',
